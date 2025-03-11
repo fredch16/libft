6657f67e04c1db7710703f38397e9041229e4caf
Fred Charbonnier <fredchar@3-E-11.42heilbronn.de>
2025-03-11 17:53:34 +0100

    Norm and formatting

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 12:04:28 by fredchar          #+#    #+#             */
-/*   Updated: 2025/03/11 16:33:20 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/03/11 17:53:20 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+// struct for the linked list functions
+
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+
+// basic functions
 
 int				ft_isalnum(unsigned char c);
 int				ft_isalpha(unsigned char c);
@@ -38,35 +42,39 @@ char			*ft_strrchr(const char *s, int c);
 const char		*ft_strnstr(const char *big, const char *little, size_t len);
 int				ft_strncmp(char *s1, char *s2, unsigned int n);
 int				ft_atoi(char *str);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlcpy(char *dst, const char *src, unsigned int dstsize);
+
+// memory functions
 
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memmove(void *dest, const void *src, size_t n);
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
-unsigned int	ft_strlcpy(char *dst, const char *src, unsigned int dstsize);
+// malloc / harder functions
 
 void			*ft_calloc(size_t count, size_t size);
 char			*ft_strdup(char *str);
-
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
 char			*ft_itoa(int n);
 char			**ft_split(char const *s, char c);
 
+// functions in functions
+
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void			ft_striteri(char *s, void (*f)(unsigned int, char *));
 
-void			ft_striteri(char *s, void (*f)(unsigned int, char *));
+// FD functions
 
 void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int nb, int fd);
 
-//	bonus stuff
+//	bonus / linked list functions
 
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **lst, t_list *new);
